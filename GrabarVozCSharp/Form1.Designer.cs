b7864559c5f676b28114b945079580ce1336d0ae
KrDev <KrDev@DESKTOP-DSBL2VL>
2024-10-21 18:31:19 -0500

    First

---
+++ GrabarVozCSharp/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace GrabarVozCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.btnReproducir = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Location = new System.Drawing.Point(41, 50);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(75, 56);
+            this.btnGrabar.TabIndex = 0;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.Location = new System.Drawing.Point(137, 50);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(75, 56);
+            this.btnDetener.TabIndex = 1;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.UseVisualStyleBackColor = true;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // btnReproducir
+            // 
+            this.btnReproducir.Location = new System.Drawing.Point(236, 50);
+            this.btnReproducir.Name = "btnReproducir";
+            this.btnReproducir.Size = new System.Drawing.Size(75, 56);
+            this.btnReproducir.TabIndex = 2;
+            this.btnReproducir.Text = "Reproducir";
+            this.btnReproducir.UseVisualStyleBackColor = true;
+            this.btnReproducir.Click += new System.EventHandler(this.btnReproducir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 155);
+            this.Controls.Add(this.btnReproducir);
+            this.Controls.Add(this.btnDetener);
+            this.Controls.Add(this.btnGrabar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.Button btnReproducir;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ GrabarVozCSharp/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrabarVozCSharp
+{
+    public partial class Form1 : Form
+    {
+        string _fileName;
+        [DllImport("winmm.dll", EntryPoint = "mciSendStringA", ExactSpelling = true, CharSet = CharSet.Ansi, SetLastError = true)]
+        private static extern int mciSendString(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void grabar()
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog() { Filter = "Media (.wav)|*.wav" })
+            {
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    _fileName = sfd.FileName;
+                    mciSendString("open new Type waveaudio Alias recsound", "", 0, 0);
+                    mciSendString("record recsound", "", 0, 0);
+                    btnDetener.Enabled = true;
+                    btnGrabar.Enabled = false;
+                }
+            }
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+            grabar();
+        }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            mciSendString("save recsound " + _fileName, "", 0, 0);
+            mciSendString("close recsound", "", 0, 0);
+            btnGrabar.Enabled = true;
+            btnDetener.Enabled = false;
+        }
+
+        private void btnReproducir_Click(object sender, EventArgs e)
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(_fileName);
+            soundPlayer.Play();
+        }
+    }
+}
